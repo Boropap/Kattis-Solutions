b2ccbb376c186be6f7762e0c141cbfaa1684b01e
Caio Rangel <85188377+Boropap@users.noreply.github.com>
2023-04-28 20:33:02 -0300

    Update Undead_Or_Alive.py

--- Python Kattis/Easy/Undead_Or_Alive.py
+++ Python Kattis/Easy/Undead_Or_Alive.py
@@ -1,3 +1,15 @@
+'''
+Input
+The input will consist of a single line of text, no longer than 
+ characters, containing letters, numbers, spaces, and any of the following symbols: .,'!?:)(.
+
+Output
+If the line contains only smiley faces and no frowny faces, then output alive.
+If the line contains only frowny faces and no smiley faces, then output undead.
+If the line contains both smiley faces and frowny faces, then output double agent.
+Otherwise, output machine.
+'''
+
 x  = input()
 
 if (":)" in x) == True and (":(" in x) == True:
