ad142add6d866bc3b9b7a7790452a57c7a879a73
Caio Rangel <85188377+Boropap@users.noreply.github.com>
2023-05-02 20:08:29 -0300

    Update Stuck_In_A_Time_Loop.py

--- Python Kattis/Easy/Stuck_In_A_Time_Loop.py
+++ Python Kattis/Easy/Stuck_In_A_Time_Loop.py
@@ -1,3 +1,11 @@
+'''
+Input
+Input consists of a single integer N (1 <= N <= 100).
+
+Output
+Output the entire wizard’s spell by counting from 
+to N, giving one number and “Abracadabra” per line.
+'''
 x = int(input())
 y = 1
 while (x >= y):
